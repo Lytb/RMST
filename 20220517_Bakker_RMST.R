853c9d8167c0e188a87579a46e8cb6f1d5ed9642
Lytske <lytskebakker@gmail.com>
2022-05-22 21:13:20 +0200

    Add files via upload

--- 20220517_Bakker_RMST.R
+++ 20220517_Bakker_RMST.R
@@ -2,136 +2,132 @@
 ###################################Load packages################################
 ################################################################################
     rm(list = ls())
-    pacman::p_load(ggplot2, survRM2,survival,flexsurv,RISCA,survminer)
+    pacman::p_load(survRM2,survival,flexsurv,RISCA)
 
 ################################################################################
 ####################################Load data###################################
 ################################################################################
 #Load Colon Dataset available in survival package.
-    data_surv <- colon[colon$etype==2,c("rx","age","time","status")]
+    data_surv <- colon[colon$etype==2 & c(colon$rx== "Obs"|colon$rx== "Lev"),c("rx","age","time","status")]
 
-################################################################################
-############################RMST WITHOUT FUNCTIONS##############################
-#Fit models#####################################################################
-#save the distributions that will be fitted as a vector            
-    distributions <- c("exp","weibull", "gompertz","gamma","lnorm", "llogis")
-    
-    Models_st <- list(length(distributions))
-        for(i in 1:length(distributions)){
-            fitted_distrsp<- tryCatch({flexsurvreg(formula = Surv(time,status) ~ 1, data= subset(data_surv,rx=="Obs"), dist = paste(distributions[i]))},error=function(e){"NULL"})#use tryCatch to address the issue that sometimes models don't converge.
-            Models_st[[i]]<-fitted_distrsp
-    }
-    
 
-#ExtractData for Plot###########################################################
-#https://stackoverflow.com/questions/52001230/r-flexsurv-estimating-a-survival-function
-    pred <- setNames(data.frame(matrix(ncol = length(distributions)+1, nrow = length(seq(0, 3000, by=1)))), c("time", paste(distributions)))
-    pred[,1] <-seq(0, 3000, by=1)#select the time for which to plot survival
-        for (i in 1:length(distributions)) {
-            pred[,i+1] <- summary(Models_st[[i]],t=seq(0, 3000, by=1))[[1]][,"est"]#Generate the new values for the novel timepoints
-          }  
+
+#Function.Model Fit#############################################################
+ 
+f_fittedmodels<-   function(distr,data_colon, treatment){
+                   surv_model <- function(distr){tryCatch({flexsurvreg(formula = Surv(time,status) ~ 1, data= data_colon[data_colon$rx ==treatment,], dist = paste(distr))}, error= function(e){"NULL"})
+                        }
+                        
+                   ModelOutput <- lapply(distr,surv_model)
+                   return(ModelOutput)
+      }
+     
+    
+distributions <- c("exp","weibull", "gompertz","gamma","lnorm", "llogis")
+Models_st_func <- f_fittedmodels(distributions,data_surv, "Obs")
+
+#Function.Extract Data for Plot#################################################
+
+f_pred <- function(distr, models_fitted){
+
+          prediction <- function(models_fitted)summary(models_fitted,t=seq(0, 5000, by=1))[[1]][,"est"]               pred <- lapply(models_fitted,prediction)
           
-      
-
-#Plots##########################################################################
-    labels <- c("KM curve",paste(distributions))#labels for the plot
-    r_col <- rainbow(length(distributions))#select the colours for the curves
-    plot(survfit(Surv(time,status)~1, data = data_surv[data_surv$rx== "Obs",]), lty = c(1),xlab = "Time in Days", ylab = "Overall Survival", main= "Overall Survival for Observation Arm", xlim=c(0.01, 3000), conf.int = FALSE)#Plot the KM estimate
-        for (i in 2:c(length(distributions)+1)){#plot the extrapolated survival curves for each distribution
-            lines(x=pred[,1],y=pred[,i], col = r_col[i], lty = c(1))
+          return(pred)
     }
-    legend("topright", legend = labels, col = c("black",r_col[1:length(distributions)]), lty = rep(1,length(distributions)+1), cex = 0.5, bty="n")#add the legend
-
-#rmst parametric################################################################
-    set.seed(1) # to ensure comparability between what is done here and when using a function to estimate these results
-    rmst_all <- data.frame(matrix(nrow = 1, ncol = 5)) #create data.frame to store results and name columns
-    colnames(rmst_all) <- c("model","time","est","lci","uci") 
     
-        for (i in 1:length(distributions)){
-            rmst_ <- c(c(paste(distributions[i])),round(unlist(print(summary(Models_st[[i]],type = "rmst", t=2190)[[1]])),4)) #create object that includes name of the model fitted and the rmst (incl lower and upperci)
-            rmst_all[i,] <- rmst_
+pred_func <- f_pred(distributions,Models_st_func)
+
+#plotCIs for Plot
+f_pred_lci <- function(distr, models_fitted){
+  
+          prediction_lci <- function(models_fitted)summary(models_fitted,t=seq(0, 5000, by=1))[[1]][,"lcl"]
+          pred_lci <- lapply(models_fitted,prediction_lci)
+          return(pred_lci)
+}
+
+pred_func_lci <- f_pred_lci(distributions,Models_st_func)
+
+
+f_pred_uci <- function(distr, models_fitted){
+  
+          prediction_uci <- function(models_fitted)summary(models_fitted,t=seq(0, 5000, by=1))[[1]][,"ucl"]
+          pred_uci <- lapply(models_fitted,prediction_uci)
+          return(pred_uci)
+}
+
+pred_func_uci <- f_pred_uci(distributions,Models_st_func)
+
+#Function.Plot##################################################################
+f_plot <- function(distr,data_colon, treatment, title,pred,pred_lci,pred_uci){
+
+          labels <- c("KM curve",paste(distr))
+          r_col <- rainbow(length(distr))
+          p1 <- plot(survfit(Surv(time,status)~1, data = data_colon[data_colon$rx==treatment,]), lty = c(1), xlab = "Time in Days", ylab = "Overall Survival", main= title, xlim=c(0.01, 5000), conf.int = T)
+                    for (i in 1:c(length(distr))){
+                    lines(x=seq(0, 5000, by=1),y=pred[[i]], col = r_col[i], lty = c(1))
+                    }
+          legend("topright", legend = labels, col = c("black",r_col[1:length(distr)]), lty = rep(1,length(distr)+1), cex = 0.5, bty="n")
+      
+          return(p1)
+
+      }
+
+plot_func <- f_plot(distributions, data_surv, "Obs","Overall Survival for Observation Arm",pred_func, pred_func_lci,pred_func_uci)
+
+#Plot with CIs
+f_plot_withCI <- function(distr,data_colon, treatment, title,pred,pred_lci,pred_uci){
+  
+            labels <- c("KM curve",paste(distr))
+            r_col <- rainbow(length(distr))
+            p1 <- plot(survfit(Surv(time,status)~1, data = data_colon[data_colon$rx==treatment,]), lty = c(1),xlab = "Time in Days", ylab = "Overall Survival", main= title, xlim=c(0.01, 5000), conf.int = T)
+            for (i in 1:c(length(distr))){
+              lines(x=seq(0, 5000, by=1),y=pred[[i]], col = r_col[i], lty = c(1))
+            }
+            for (i in 1:c(length(distr))){
+              lines(x=seq(0, 5000, by=1),y=pred_lci[[i]], col = r_col[i], lty = c(2))
+            }
+            for (i in 1:c(length(distr))){
+              lines(x=seq(0, 5000, by=1),y=pred_uci[[i]], col = r_col[i], lty = c(2))
+            }
+            legend("topright", legend = labels, col = c("black",r_col[1:length(distr)]), lty = rep(1,length(distr)+1), cex = 0.5, bty="n")
+            
+            return(p1)
+  
+}
+
+plot_func_withCI <- f_plot_withCI(distributions, data_surv, "Obs","Overall Survival for Observation Arm",pred_func, pred_func_lci,pred_func_uci)
+
+#Function.RMST##################################################################
+f_rmst <- function(fittedm,rmst_time){
+          set.seed(1)
+          f_est_rmst <- function(fittedm){tryCatch({round(unlist(print(summary(fittedm,type = "rmst", t=c(rmst_time))[[1]])),3)}, error=function(e) {"NULL"})}
+              
+          rmst_all <- lapply(fittedm, f_est_rmst)
+          return(rmst_all)
     }
+    
+rmst_all_func <- f_rmst(Models_st_func,2190)
+attributes(rmst_all_func) <- list(names= paste(distributions),row.names=c("time","est","lci","uci"),class="data.frame")
+formattable(rmst_all_func)
+
 
 #RMST KM survRM2################################################################
 #https://cran.r-project.org/web/packages/survRM2/vignettes/survRM2-vignette3-2.html
-#data_surv_sub <- data_surv[data_surv$rx=="Obs",]
-    data_surv_sub <- data_surv
-    levels(data_surv_sub$rx) <- c(0,1,2)
-    set.seed(1)
-    obj <- rmst2(time=data_surv_sub$time,status = data_surv_sub$status, arm=data_surv_sub$rx,tau=2190)
-    rmst_all[length(distributions)+1,] <- c("SurvRM2",2190,round(obj[[4]]$rmst[1],4),round(obj[[4]]$rmst[3],4),round(obj[[4]]$rmst[4],4))
+
+levels(data_surv$rx) <- c("0","1","3")
+data_surv$rx <- as.numeric(as.character(data_surv$rx))
+
+set.seed(1)
+obj <- rmst2(time=data_surv$time,status = data_surv$status, arm=data_surv$rx,tau=2190)
+SurvRM2_rmst <- c(round(obj[[4]]$rmst[1],4),round(obj[[4]]$rmst[3],4),round(obj[[4]]$rmst[4],4))
 
 #RMST KM RISCA##################################################################
 #Identical to SurvRM2 but doesn't report Confidence intervals
-    km_colon <- summary(survfit(Surv(time,status) ~ 1, data= data_surv[data_surv$rx== "Obs",]))
-    rmst_all[c(length(distributions)+2),] <- c("Risca rmst",2190,round(RISCA::rmst(km_colon$time,km_colon$surv,max.time = 2190),4),"","")
 
+km_colon <- summary(survfit(Surv(time,status) ~ 1, data= data_surv[data_surv$rx== 0,]))
+risca_rmst <- round(RISCA::rmst(km_colon$time,km_colon$surv,max.time = 2190),4)
 
-####################################Functions###################################
-#Function.Model Fit#############################################################
-    f_fittedmodels<-   function(distr,data_colon, treatment){
   
-    ModelOutput <- list(length(distr))
-        for(i in 1:length(distr)){
-            fitted_distr<- tryCatch({flexsurvreg(formula = Surv(time,status) ~ 1, data= data_colon[data_colon$rx ==treatment,], dist = paste(distr[i]))}, error= function(e){"NULL"})#fit for each of the distributions
-            ModelOutput[[i]] <- fitted_distr #save the distributions
-        }
-        return(ModelOutput)
-  
-  }
-
-
-
-#Function.Extract Data for Plot#################################################
-    f_pred <- function(distr, models_fitted){
-    pred[,1] <-seq(0, 3000, by=1)
-  
-        for (i in 1:length(distr)) {
-            pred[,i+1] <- summary(models_fitted[[i]],t=seq(0, 3000, by=1))[[1]][,"est"]#Generate the new values for the novel timepoints
-        }  
-        return(pred)
-  }
-
-
-
-#Function.Plot##################################################################
-    f_plot <- function(distr,data_colon, treatment, title,pred){
-
-        labels <- c("KM curve",paste(distr))
-        r_col <- rainbow(length(distr))
-        p1 <- plot(survfit(Surv(time,status)~1, data = data_colon[data_colon$rx==treatment,]), lty = c(1),xlab = "Time in Days", ylab = "Overall Survival", main= title, xlim=c(0.01, 3000), conf.int = FALSE)
-            for (i in 2:c(length(distr)+1)){
-            lines(x=pred[,1],y=pred[,i], col = r_col[i], lty = c(1))
-            }
-        legend("topright", legend = labels, col = c("black",r_col[1:length(distr)]), lty = rep(1,length(distr)+1), cex = 0.5, bty="n")
-      
-        return(p1)
-
-    }
-
-
-
-#Function.RMST##################################################################
-    f_rmst <- function(distr,fittedm,rmst_time){
-    set.seed(1)
-    rmst_all <- data.frame(matrix(nrow = length(distr), ncol = 5))
-    colnames(rmst_all) <- c("model","time","est","lci","uci") #Add column name
-        for (i in 1:length(fittedm)){
-            rmst_all[i,] <- c(paste(distributions[i]),tryCatch({round(unlist(print(summary(fittedm[[i]],type = "rmst", t=c(rmst_time))[[1]])),3)
-    }, error=function(e) {"NULL"}))
-        }
-        return(rmst_all)
-    }
-    
-##############################RESULTS USING FUNCTIONS###########################
-#Standard parametric distributions in flexsurv##################################
-            
-    distributions <- c("exp","weibull", "gompertz","gamma","lnorm", "llogis")
-    Models_st_func <- f_fittedmodels(distributions,data_surv, "Obs")
-    pred_func <- f_pred(distributions,Models_st_func)
-    plot_func <- f_plot(distributions, data_surv, "Obs","Overall Survival for Observation Arm",pred_func)
-    rmst_all_func <- f_rmst(distributions,Models_st_func,2190)
-
 
 ################################ADDITIONAL INFO#################################
 #Restricted vs mean survival time###############################################
@@ -145,3 +141,7 @@
 
 #RMST:Restricted Mean Survival Time Methods Jonathan Wessen
 #https://rdrr.io/github/scientific-computing-solutions/RMST/f/inst/doc/vignette.pdf
+    
+#Extrapolating values in plots
+#https://stackoverflow.com/questions/52001230/r-flexsurv-estimating-a-survival-function
+    
